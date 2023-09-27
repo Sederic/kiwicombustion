482b6209b13876f1d6e87354e148a5d601eacc37
Sederic <sergioandresdelrio@gmail.com>
2023-09-26 22:25:13 -0700

    Correct version of Unity

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Player : MonoBehaviour
+{
+    #region Player Variables
+    [SerializeField] private float thurstForce;
+    [SerializeField] private float forwardThrust;
+    [SerializeField] private float glideEffect;
+    [SerializeField] private float standardPlayerGravity;
+    private bool jetpackActive;
+    private Rigidbody2D myRigidbody;
+    private Animator myAnimator;
+    [SerializeField] private ParticleSystem myParticleSystem;
+    private float playerAltitude;
+    [SerializeField] TMP_Text altitudeText;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        JetpackThurst();
+        Glide();
+        TrackAltitude();
+    }
+
+    private void TrackAltitude()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2 (transform.position.x, transform.position.y - 5), Vector2.down);
+        if (hit.collider != null && hit.collider.transform.tag == "Ground")
+        {
+            playerAltitude = Mathf.Abs(hit.point.y - transform.position.y);
+            altitudeText.text = "Altitude: \n   " + Mathf.Round(playerAltitude);
+        }
+    }
+
+    private void JetpackThurst()
+    {
+        //if Space Bar is being pressed then cause jetpack thrust (& set jetpackActive to true)
+        if (Input.GetKey(KeyCode.Space))
+        {
+            myRigidbody.AddForce(new Vector3 (forwardThrust, thurstForce,0));
+            jetpackActive = true;
+            myAnimator.SetBool("IsFlying", true);
+            myParticleSystem.Play();
+        }
+        else
+        {
+        //if Space Bar is not being pressed then jetpack is not active.
+            jetpackActive = false;
+            myAnimator.SetBool("IsFlying", false);
+            myParticleSystem.Stop();
+        }
+    }
+
+    private void Glide()
+    {
+        //If Left Shift is pressed AND Jetpack is NOT active, then lower gravity and glide.
+        if (Input.GetKey(KeyCode.LeftShift) && !jetpackActive)
+        {
+            myRigidbody.gravityScale = glideEffect;
+            myRigidbody.velocity = new Vector2(forwardThrust, 0);
+            myAnimator.SetBool("IsGliding", true);
+        }
+        else
+        {
+        // if Left-Shift is not being pressed, then gravity is normal
+            myRigidbody.gravityScale = standardPlayerGravity;
+            myAnimator.SetBool("IsGliding", false) ;
+        }
+    }
+}
